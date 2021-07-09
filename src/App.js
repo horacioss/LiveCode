73767b676ab7932470a4cffdabf57870baf695d0
Horacio <horacioss2013@gmail.com>
2021-07-08 21:58:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+
+import React, {useState, useEffect} from "react";
+
 import './App.css';
 
+import {getData} from "./getData";
+
 function App() {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    
+    const getUserData = async () => {
+      
+      const users = await getData();
+      
+      setData(users);
+
+    };
+
+    getUserData()  
+
+
+  }, [data])
+
+
+  const sortBy = (name) => {  
+    return data.sort((x, y) => x.location[name].localeCompare(y.location[name]));   
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <input type='text'/>
+      <table> 
+        <th>
+          <tr>
+            <td onClick={()=>setData(sortBy('city'))} >City</td>
+            <td onClick={()=>sortBy('state')}>State</td>
+            <td onClick={()=>sortBy('country')}>Country</td>
+            <td onClick={()=>sortBy('postcode')}>PostCode</td>
+            <td onClick={()=>sortBy('city')}>Number</td>
+            <td onClick={()=>sortBy('city')}>Name</td>
+            <td onClick={()=>sortBy('city')}>Latitude</td>
+            <td onClick={()=>sortBy('city')}>Longitude</td>
+          </tr>
+        </th>
+        <tbody>
+          {
+            data?.map((el, i) => {
+              return  (
+                <tr  key={i}>
+                     <td>{el.location.city}</td>
+                     <td>{el.location.state}</td>
+                     <td>{el.location.country}</td>
+                     <td>{el.location.postcode}</td>
+                     <td>{el.location.street.number}</td>
+                     <td>{el.location.street.name}</td>
+                     <td>{el.location.coordinates.latitude}</td>
+                    <td>{el.location.coordinates.longitude}</td>
+                 </tr>
+                )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/getData.js
@@ -0,0 +1,10 @@
+import axios  from "axios";
+
+export const getData = async () => {
+    const url = 'https://randomuser.me/api/?results=20';
+
+    const {data} = await axios.get(url);
+    
+    return data.results;
+}
+
