bbfbc460c02095f319184883d9dc9f1b78bf5fc1
Horacio <horacioss2013@gmail.com>
2021-07-09 10:16:53 -0400

    ..

--- src/App.js
+++ src/App.js
@@ -1,70 +1,56 @@
+import React, { useState, useEffect } from "react";
 
-import React, {useState, useEffect} from "react";
+import "./App.css";
 
-import './App.css';
-
-import {getData} from "./getData";
+import { getData } from "./getData";
 
 function App() {
-
   const [data, setData] = useState();
 
   useEffect(() => {
-    
     const getUserData = async () => {
-      
       const users = await getData();
-      
       setData(users);
-
     };
 
-    getUserData()  
+    getUserData();
+  }, []);
 
-
-  }, [data])
-
-
-  const sortBy = (name) => {  
-    return data.sort((x, y) => x.location[name].localeCompare(y.location[name]));   
-  }
-
-
+  const sortBy = (name) => {
+    return data.sort((x, y) =>
+      x.location[name].localeCompare(y.location[name])
+    );
+  };
 
   return (
     <div className="App">
-
-      <input type='text'/>
-      <table> 
-        <th>
-          <tr>
-            <td onClick={()=>setData(sortBy('city'))} >City</td>
-            <td onClick={()=>sortBy('state')}>State</td>
-            <td onClick={()=>sortBy('country')}>Country</td>
-            <td onClick={()=>sortBy('postcode')}>PostCode</td>
-            <td onClick={()=>sortBy('city')}>Number</td>
-            <td onClick={()=>sortBy('city')}>Name</td>
-            <td onClick={()=>sortBy('city')}>Latitude</td>
-            <td onClick={()=>sortBy('city')}>Longitude</td>
-          </tr>
-        </th>
+      <input type="text" />
+      <table>
+        <tr>
+          <th onClick={() => setData(sortBy("city"))}>City</th>
+          <th onClick={() => setData(sortBy("city"))}>State</th>
+          <th onClick={() => setData(sortBy("city"))}>Country</th>
+          <th onClick={() => setData(sortBy("city"))}>Postcode</th>
+          <th onClick={() => setData(sortBy("city"))}>Number</th>
+          <th onClick={() => setData(sortBy("city"))}>Name</th>
+          <th onClick={() => setData(sortBy("city"))}>Latitude</th>
+          <th onClick={() => setData(sortBy("city"))}>Longitude</th>
+        </tr>
         <tbody>
-          {
-            data?.map((el, i) => {
-              return  (
-                <tr  key={i}>
-                     <td>{el.location.city}</td>
-                     <td>{el.location.state}</td>
-                     <td>{el.location.country}</td>
-                     <td>{el.location.postcode}</td>
-                     <td>{el.location.street.number}</td>
-                     <td>{el.location.street.name}</td>
-                     <td>{el.location.coordinates.latitude}</td>
-                    <td>{el.location.coordinates.longitude}</td>
-                 </tr>
-                )
-            })
-          }
+          {data?.map((el, i) => {
+            return (
+              <tr key={i}>
+                <td>{el.location.city}</td>
+                <td>{el.location.state}</td>
+                <td>{el.location.country}</td>
+                <td>{el.location.postcode}</td>
+                <td>{el.location.street.number}</td>
+                <td>{el.location.street.name}</td>
+                <td>{el.location.coordinates.latitude}</td>
+                <td>{el.location.coordinates.longitude}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
